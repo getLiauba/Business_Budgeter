095d4f6422b44704216d2a6d3fb1380b436bc07e
andrewliauba <iamliauba@hotmail.com>
2020-07-03 17:32:25 -0400

    Added login button on click listener

--- app/src/main/java/com/example/business_budgeter/Login.java
+++ app/src/main/java/com/example/business_budgeter/Login.java
@@ -6,19 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
 
     TextView lRegisterBtn;
-
+    Button login_Btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         lRegisterBtn = findViewById(R.id.GoTo_Register);
-
+        login_Btn = findViewById(R.id.lLoginBtn);
 
         lRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +27,15 @@ public class Login extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),Register.class));
             }
         });
+
+        login_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+
 
 
 
